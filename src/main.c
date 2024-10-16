96fb94ed018ee439f08f823631e08cf907a635cc
ygrayque <ygrayque@gmail.com>
2024-10-16 03:32:40 +0200

    trop de modif pour tout lister

--- src/main.c
+++ src/main.c
@@ -4,8 +4,6 @@
 #include <time.h>
 #include "password_manager.h"
 
-void list_password_names();
-
 int main() {
     srand(time(NULL));
 
@@ -19,26 +17,44 @@ int main() {
     fclose(key_file);
 
     int choice;
-    printf("1. Generate password\n2. Retrieve password\n");
+    printf("======================================\n");
+    printf("        Password Manager\n");
+    printf("======================================\n");
+    printf("1. Generate password\n");
+    printf("2. Retrieve password\n");
+    printf("Please enter your choice: ");
     scanf("%d", &choice);
 
     if (choice == 1) {
         char password[MAX_PASSWORD_LENGTH + 1];
         generate_password(password);
-        printf("Generated password: %s\n", password);
+        printf("\nGenerated password: %s\n", password);
         char name[256];
         printf("Enter name for the password: ");
         scanf("%255s", name);
+        if (strlen(name) == 0) {
+            printf("Error: Password name cannot be empty.\n");
+            return 1;
+        }
         save_password(name, password, key);
+        printf("Password saved successfully!\n");
     } else if (choice == 2) {
-        list_password_names();
-        char name[256];
-        printf("Enter name of the password to retrieve: ");
-        scanf("%255s", name);
-        retrieve_password(name, key);
+        if (!list_password_names()) {
+            printf("No passwords are saved.\n");
+        } else {
+            char name[256];
+            printf("Enter name of the password to retrieve: ");
+            scanf("%255s", name);
+            if (strlen(name) == 0) {
+                printf("Error: Password name cannot be empty.\n");
+                return 1;
+            }
+            retrieve_password(name, key);
+        }
     } else {
-        printf("Invalid choice.\n");
+        printf("Invalid choice. Please enter 1 or 2.\n");
     }
 
+    printf("======================================\n");
     return 0;
 }
