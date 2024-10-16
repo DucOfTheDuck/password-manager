bedc2477d733f92eb31dba683d02ad92f1a24575
ygrayque <ygrayque@gmail.com>
2024-10-16 02:58:54 +0200

    add + de caractères

--- src/password_generator.c
+++ src/password_generator.c
@@ -3,7 +3,7 @@
 #include <time.h>
 
 void generate_password(char *password) {
-    const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$^&*()";
+    const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#$&'()*+,-./:;<=>?@[]^_`{|}~";
     int length = MIN_PASSWORD_LENGTH + rand() % (MAX_PASSWORD_LENGTH - MIN_PASSWORD_LENGTH + 1);
     for (int i = 0; i < length; i++) {
         password[i] = charset[rand() % (sizeof(charset) - 1)];
